d5cb281b6196860a71c63f5ab8fba3c93e86bd59
LagrangeRomain <romain.lagrange@outlook.fr>
2025-05-14 22:18:35 +0200

    Day 2 push

---
+++ beginner/Day 2 - Tip Calculator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give?"))
+people = int(input("How many people to split the bill? "))
+result = round((bill + bill*tip/100) /people,2)
+print(f"Each person should pay: ${result}")
