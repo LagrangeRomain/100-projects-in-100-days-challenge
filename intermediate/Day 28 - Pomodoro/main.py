3e0504f5099d3b7a5cad023b212ba28672ce93c3
LagrangeRomain <romain.lagrange@outlook.fr>
2025-06-16 21:58:34 +0200

    Day 28

--- intermediate/Day 28 - Pomodoro/main.py
+++ intermediate/Day 28 - Pomodoro/main.py
@@ -1,8 +1,4 @@
 from tkinter import *
-import time
-
-from deep_translator.constants import BASE_URLS
-from openpyxl.cell.text import PhoneticProperties
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -15,11 +11,32 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+reps = 0
+timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global reps
+    reps = 0
+    section_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    pomodoro_label.config(text="")
+    window.after_cancel(timer)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(65)
+    global reps
+    reps += 1
+    if reps % 8 == 0:
+        count_down(LONG_BREAK_MIN * 60)
+        section_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0 :
+        count_down(SHORT_BREAK_MIN * 60)
+        section_label.config(text="Break", fg=PINK)
+    else:
+        count_down(WORK_MIN * 60)
+        section_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
@@ -27,7 +44,14 @@ def count_down(count):
     second = str(count % 60).zfill(2)
     canvas.itemconfig(timer_text, text=f"{minute}:{second}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        global reps
+        if reps % 2 != 0:
+            new_text = pomodoro_label.cget("text") + "✔"
+            pomodoro_label.config(text=new_text)
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -35,8 +59,8 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
-timer_label.grid(column=1, row=0)
+section_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+section_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
@@ -47,10 +71,10 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
-pomodoro_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+pomodoro_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
 pomodoro_label.grid(column=1, row=3)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
